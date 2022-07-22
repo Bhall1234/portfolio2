76281f7f836a2765a281be66c9b365fdcf05961e
Bhall1234 <ben.hall17@outlook.com>
2022-07-22 20:02:32 +0100

    Need to find a darker background

--- my-portfolio/src/components/header/Header.jsx
+++ my-portfolio/src/components/header/Header.jsx
@@ -3,7 +3,14 @@ import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+      <h5>Hello I'm </h5>
+      <h1>Benjamin Hall</h1>
+      <h5 className="text-light">Computer Science Student</h5>
+      
+      </div>
+    </header>
   )
 }
 
